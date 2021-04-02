3c83b5d63c6b6eb169ea5f4bb804f57e1fad113f
Kevin Robinson <kevin.robinson@outlook.com>
2021-04-02 12:22:41 +0100

    Added Database

--- WeatherApi/WeatherApi/WeatherApi/Startup.cs
+++ WeatherApi/WeatherApi/WeatherApi/Startup.cs
@@ -10,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using WeatherApi.Services;
+using WeatherApp.Data;
 
 namespace WeatherApi
 {
@@ -32,6 +34,9 @@ namespace WeatherApi
                        .AllowAnyMethod()
                        .AllowAnyHeader();
             }));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("WeatherDbConnection")));
 
             services.AddScoped<IWeatherService, WeatherService>();
 
---
+++ WeatherApi/WeatherApi/WeatherApp.Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WeatherApp.Data
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+    }
+}
